f696fef18d3c6f23f58094f0c172e74360b1b449
Igor Lukjanov <lukjanov.igr@gmail.com>
2023-11-27 20:58:27 +0200

    add development and production env variables

--- 4-natours-api/src/index.js
+++ 4-natours-api/src/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 const TourRouter = require('./routes/tour.router');
 const UserRouter = require('./routes/user.router');
-const morgan = require('morgan');
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // const auth = (req, res, next) => {
 //   console.log('got you');
 //   next();
 // };
 
-app.use(morgan('dev'));
 app.use(express.json());
 // app.use(auth);
+app.use(express.static(`public`));
 app.use('/api/users', UserRouter);
 app.use('/api/tours', TourRouter);
 
--- 4-natours-api/src/server.js
+++ 4-natours-api/src/server.js
@@ -1,6 +1,9 @@
+require('dotenv').config({ path: '.env.development' });
 const app = require('./index');
 
-const PORT = 5000;
+console.log('environment', process.env.NODE_ENV);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server has start running on PORT ${PORT}`);
 });
