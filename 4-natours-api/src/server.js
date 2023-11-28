e21c063d42c285bfa3c9dedc3da342d5f6176859
Igor Lukjanov <lukjanov.igr@gmail.com>
2023-11-28 18:54:04 +0200

    update package.json for different environments

--- 4-natours-api/src/server.js
+++ 4-natours-api/src/server.js
@@ -1,7 +1,5 @@
 require('dotenv').config({ path: '.env.development' });
-const app = require('./index');
-
-console.log('environment', process.env.NODE_ENV);
+const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
